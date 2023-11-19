79b793ae84b166d882c5827335d8c639f2d03094
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-11-19 03:38:11 +0300

    add debt_nullification

--- electronics_retail/admin.py
+++ electronics_retail/admin.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.contrib import admin
 
 from electronics_retail.models import Retail, Product, Contact, Address, RetailType
@@ -6,12 +5,19 @@ from electronics_retail.models import Retail, Product, Contact, Address, RetailT
 
 @admin.register(Retail)
 class RetailAdmin(admin.ModelAdmin):
-
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
         if obj and obj.type == RetailType.FACTORY:
             form.base_fields['provider'].queryset = Retail.objects.none()
         return form
+
+    @admin.action(description="Debt nullification")
+    def debt_nullification(self, request, queryset):
+        print(queryset)
+        queryset.update(debt=0)
+        return queryset
+
+    actions = (debt_nullification, )
 
 
 @admin.register(Product)
