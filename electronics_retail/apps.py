c12b7d8c48c78cd5dd01158dc7c314fc860e034d
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-11-20 03:46:02 +0300

    add admins import to app settings

--- electronics_retail/apps.py
+++ electronics_retail/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ElectronicsRetailConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'electronics_retail'
+
+    def ready(self):
+        from .admins import AddressAdmin, ContactAdmin, ProductAdmin, RetailAdmin
