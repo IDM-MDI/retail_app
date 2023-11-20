ad1a1e0cee16bd8968cbd1bd9be66f54291a921c
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-11-20 03:18:47 +0300

    add high_debt_avg.py

---
+++ electronics_retail/views/high_debt_avg.py
@@ -0,0 +1,16 @@
+from django.db import models
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+
+from electronics_retail.models import Retail
+from electronics_retail.serializers.retail_serializer import RetailSerializer
+
+
+class RetailModelSet(generics.ListAPIView):
+    queryset = Retail.objects.all()
+    serializer_class = RetailSerializer
+    permission_classes = (IsAdminUser, )
+
+    def get_queryset(self):
+        average_debt = Retail.objects.aggregate(avg_debt=models.Avg('debt'))['avg_debt']
+        return Retail.objects.filter(debt__gt=average_debt)
